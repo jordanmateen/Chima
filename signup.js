596da0613030520eaba98f4ef02b25a419b52534
Jordan Mateen <jordan.mateen@coxautoinc.com>
2024-03-02 13:11:08 -0500

    tracking sign up page.

---
+++ signup.js
@@ -0,0 +1,13 @@
+// sign up page variable. 
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+
+// Sign Up Page
+
+firstName.addEventListener('change', (event)=>{
+    console.log(event)
+})
